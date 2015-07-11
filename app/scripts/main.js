34a083a730fd982aa412a99466ffd332b66442e2
Phillip Creglow <phillipcreglow@msn.com>
2015-07-11 14:12:16 -0400

    Route for contact.html in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,13 +11,17 @@
     $routeProvider.when('/home', {
       templateUrl: 'views/home.html'
     });
-    
+
     $routeProvider.when('/lakeEola', {
       templateUrl: 'views/lakeEola.html'
     });
 
     $routeProvider.when('/millsFifty', {
       templateUrl: 'views/millsFifty.html'
+    });
+
+    $routeProvider.when('/contact', {
+      templateUrl: 'views/contact.html'
     });
 
     $routeProvider.when('/404', {
