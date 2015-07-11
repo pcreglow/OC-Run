ca3e1bd0f50dfc8942b9fe89efd0effbb924a448
Phillip Creglow <phillipcreglow@msn.com>
2015-07-10 22:35:59 -0400

    Removed ng-controller and routes are working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+/* global angular Firebase */
+(function(){
+  'use strict';
+
+  angular.module('oc-run', [
+    'ngRoute',
+    'restangular',
+    'firebase'
+  ])
+  .config(function($routeProvider){
+    $routeProvider.when('/lakeEola', {
+      templateUrl: 'views/lakeEola.html'
+    });
+
+    $routeProvider.when('/millsFifty', {
+      templateUrl: 'views/millsFifty.html'
+    });
+
+    $routeProvider.when('/404', {
+      templateUrl: 'views/404.html'
+    });
+
+    
+
+  })
+})();
