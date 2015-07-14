c31d030baaf5bf9e4b5c7df72780dd4da3c181e9
Phillip Creglow <phillipcreglow@msn.com>
2015-07-14 14:23:16 -0400

    Added LakeController to main.js and lakeEola.js

--- app/scripts/lakeEola.js
+++ app/scripts/lakeEola.js
@@ -1,3 +1,9 @@
 (function(){
-
-});
+angular.module('oc-run')
+  .controller('LakeController', function($scope, $http) {
+    $http.get('http://oc-run.firebaseio.com/lakeEola/.json')
+    .then(function(response){
+      console.log(response.data)
+    })
+  })
+})();
