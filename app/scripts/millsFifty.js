603d7729b328f4a76a71a2ead61a95c5da66b12e
Phillip Creglow <phillipcreglow@msn.com>
2015-07-15 09:24:18 -0400

    Added MillsController to millsFifty.js

--- app/scripts/millsFifty.js
+++ app/scripts/millsFifty.js
@@ -1,3 +1,10 @@
 (function(){
-
+  angular.module('oc-run')
+    .controller('MillsController', function($scope, $http) {
+      $http.get('http://oc-run.firebaseio.com/millsFifty/.json')
+      .then(function(response){
+        $scope.routes = response.data;
+        console.log(response.data);
+      })
+    })
 })();
