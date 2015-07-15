7d4651f04a18e35b2d6078a4c52c6dc32a09aae3
Phillip Creglow <phillipcreglow@msn.com>
2015-07-15 09:33:45 -0400

    Refactored controller in millsFifty.js and added ng-repeat

--- app/scripts/millsFifty.js
+++ app/scripts/millsFifty.js
@@ -1,7 +1,7 @@
 (function(){
   angular.module('oc-run')
     .controller('MillsController', function($scope, $http) {
-      $http.get('http://oc-run.firebaseio.com/millsFifty/.json')
+      $http.get('http://oc-run.firebaseio.com/mills50/.json')
       .then(function(response){
         $scope.routes = response.data;
         console.log(response.data);
