999da7551d2623818d2eb748d13517d9a41f7073
Phillip Creglow <phillipcreglow@msn.com>
2015-07-15 12:16:18 -0400

    Changed  path in controller to https

--- app/scripts/lakeEola.js
+++ app/scripts/lakeEola.js
@@ -4,7 +4,7 @@
 
 angular.module('oc-run')
   .controller('LakeController', function($scope, $http) {
-    $http.get('http://oc-run.firebaseio.com/lakeEola/.json')
+    $http.get('https://oc-run.firebaseio.com/lakeEola/.json')
     .then(function(response){
       $scope.routes = response.data;
       console.log(response.data);
