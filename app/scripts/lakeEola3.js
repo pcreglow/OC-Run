581e38c953877064073f293356e6caf87d90790a
Phillip Creglow <phillipcreglow@msn.com>
2015-07-16 16:50:56 -0400

    Added controller to lakeEola3.js and successful test of live firebase data

--- app/scripts/lakeEola3.js
+++ app/scripts/lakeEola3.js
@@ -2,5 +2,15 @@
 (function(){
   'use strict';
 
+  angular.module('oc-run')
+    .controller('Lake3Controller', function($http) {
+      var self = this;
+      self.routes = [ ];
 
+      $http.get('https://oc-run.firebaseio.com/lakeEola3/.json')
+      .then(function(response){
+        console.log(response.data);
+        self.routes = response.data;
+      });
+    });
 })();
