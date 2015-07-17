5b87d0ba2791cbd291e3ec854c91af09d498e078
Phillip Creglow <phillipcreglow@msn.com>
2015-07-17 13:35:23 -0400

    Added post a comment functionality to lakeEola .html and .js

--- app/scripts/lakeEola.js
+++ app/scripts/lakeEola.js
@@ -3,14 +3,24 @@
   'use strict';
 
 angular.module('oc-run')
-  .controller('LakeController', function($http) {
+  .controller('LakeController', function($http, $routeParams, $firebaseArray) {
     var self = this;
-
-
     $http.get('https://oc-run.firebaseio.com/lakeEola/.json')
     .then(function(response){
       self.routes = response.data;
       console.log(self.routes);
     });
+
+    var ref = new Firebase('https://oc-run.firebaseio.com/lakeEola/route/comments/' + $routeParams.lake);
+    self.comments = $firebaseArray(ref);
+
+    self.addComment = function(){
+
+      self.comments.$add({
+        name: self.newCommentName,
+        text: self.newCommentText
+      });
+    };
+
   });
 })();
