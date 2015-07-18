c02cd6e7f6ad5e4a62c24ff6fd09eb630af780d4
Phillip Creglow <phillipcreglow@msn.com>
2015-07-18 17:40:13 -0400

    Defined self.addComment in lakeEola2.js

--- app/scripts/lakeEola2.js
+++ app/scripts/lakeEola2.js
@@ -13,6 +13,19 @@ angular.module('oc-run')
       self.routes = response.data;
     });
 
+    var comments = $routeParams.lake;
+    var ref = new Firebase('https://oc-run.firebaseio.com/lakeEola2/route/' + 'comments');
+    self.comments = $firebaseArray(ref);
+
+    self.addComment = function(){
+
+      self.comments.$add({
+        name: self.newCommentName,
+        text: self.newCommentText
+      });
+      self.newCommentName="";
+      self.newCommentText="";
+    };
 
   });
 })();
