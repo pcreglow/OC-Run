8e0791197a1d9f83b2c10ecc93aaa9b16421a0de
Phillip Creglow <phillipcreglow@msn.com>
2015-07-18 15:49:22 -0400

    Added mills2 controller to main.js and mills2.js

--- app/scripts/mills2.js
+++ app/scripts/mills2.js
@@ -1,4 +1,15 @@
-(function() {
+/* global angular */
 
+(function() {
+  'use strict';
+
+  angular.module('oc-run')
+    .controller('Mills2Controller', function($http, $routeParams, $firebaseArray) {
+      var self = this;
+      $http.get('https://oc-run.firebaseio.com/mills2/.json')
+      .then(function(response){
+        self.routes = response.data;
+        console.log(self.routes);
+      });
 
 })();
