6a4be0adab56ae55f375d17baa8155bd4f58674a
Phillip Creglow <phillipcreglow@msn.com>
2015-07-18 16:33:26 -0400

    Added controller to mills3.js and main.js

--- app/scripts/mills3.js
+++ app/scripts/mills3.js
@@ -2,7 +2,13 @@
 (function(){
   'use strict';
 
-
-
-
+  angular.module('oc-run')
+    .controller('Mills3Controller', function($http, $routeParams, $firebaseArray) {
+      var self = this;
+      $http.get('https://oc-run.firebaseio.com/mills3/.json')
+      .then(function(response){
+        self.routes = response.data;
+        console.log(self.routes);
+      });
+    });
 })();
