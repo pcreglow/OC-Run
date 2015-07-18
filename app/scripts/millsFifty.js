868b9e27c2d9de076de99a8d59e92722497e7cbb
Phillip Creglow <phillipcreglow@msn.com>
2015-07-17 20:52:32 -0400

    Replaced  and added services to controller in lakeEola.js

--- app/scripts/millsFifty.js
+++ app/scripts/millsFifty.js
@@ -3,11 +3,18 @@
   'use strict';
 
   angular.module('oc-run')
-    .controller('MillsController', function($scope, $http) {
+    .controller('MillsController', function($http, $routeParams, $firebaseArray) {
+      var self = this;
       $http.get('https://oc-run.firebaseio.com/mills50/.json')
       .then(function(response){
-        $scope.routes = response.data;
-        console.log(response.data);
+        self.routes = response.data;
+        console.log(self.routes);
       });
+
+      var comments = $routeParams.lake;
+      var ref = new Firebase('https://oc-run.firebaseio.com/millsFifty/route/' + 'comments');
+      self.comments = $firebaseArray(ref);
+
+
     });
 })();
