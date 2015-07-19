07705d00cfd7796fa9fa42cd087f0fc67751ef32
Phillip Creglow <phillipcreglow@msn.com>
2015-07-19 16:26:08 -0400

    Added MillsEventsController to millsEvents.js

--- app/scripts/millsEvents.js
+++ app/scripts/millsEvents.js
@@ -2,7 +2,14 @@
 (function(){
   'use strict';
 
-
+  angular.module('oc-run')
+    .controller('MillsEventsController', function($http, $routeParams, $firebaseArray) {
+      var self = this;
+      $http.get('https://oc-run.firebaseio.com/millsEvents/.json')
+      .then(function(response){
+        self.routes = response.data;
+        console.log(self.routes);
+      });
 
   });
 })();
