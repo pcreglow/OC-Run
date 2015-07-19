f5307d12edd3e262b4f4eab1d9f95fecfe8635d9
Phillip Creglow <phillipcreglow@msn.com>
2015-07-19 14:31:33 -0400

    Added services to controller and defined new Firebase in lakeEolaEvents.js

--- app/scripts/lakeEolaEvents.js
+++ app/scripts/lakeEolaEvents.js
@@ -3,12 +3,25 @@
   'use strict';
 
 angular.module('oc-run')
-  .controller('LakeEventsController', function($http) {
+  .controller('LakeEventsController', function($http, $routeParams, $firebaseArray) {
     var self = this;
     $http.get('https://oc-run.firebaseio.com/lakeEvents/.json')
     .then(function(response){
       self.routes = response.data;
       console.log(self.routes);
     });
+
+    var ref = new Firebase('https://oc-run.firebaseio.com/lakeEvents/route/' + 'events');
+    self.events = $firebaseArray(ref);
+
+    self.addEvent = function(){
+
+      self.events.$add({
+        name: self.newCommentName,
+        text: self.newCommentText
+      });
+      self.newCommentName = '';
+      self.newCommentText = '';
+    };
 });
 })();
