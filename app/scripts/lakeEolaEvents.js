4c96406ecdb99279ea6ee190c137df257accd614
Phillip Creglow <phillipcreglow@msn.com>
2015-07-19 14:28:44 -0400

    Added Firebase data placeholders in description of lakeEolaEvents.html

--- app/scripts/lakeEolaEvents.js
+++ app/scripts/lakeEolaEvents.js
@@ -3,12 +3,12 @@
   'use strict';
 
 angular.module('oc-run')
-  .controller('LakeEventsController', function($http, $routeParams, $firebaseArray) {
+  .controller('LakeEventsController', function($http) {
     var self = this;
     $http.get('https://oc-run.firebaseio.com/lakeEvents/.json')
     .then(function(response){
       self.routes = response.data;
       console.log(self.routes);
     });
-
+});
 })();
