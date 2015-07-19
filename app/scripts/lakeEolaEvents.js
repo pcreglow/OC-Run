1a19a68d7bb3f306917a7fa902add034cce9e8a7
Phillip Creglow <phillipcreglow@msn.com>
2015-07-19 14:07:58 -0400

    Added lakeEvents controller to lakeEolaEvents.js

--- app/scripts/lakeEolaEvents.js
+++ app/scripts/lakeEolaEvents.js
@@ -1,3 +1,14 @@
+/* global angular Firebase */
 (function(){
+  'use strict';
+
+angular.module('oc-run')
+  .controller('LakeEventsController', function($http, $routeParams, $firebaseArray) {
+    var self = this;
+    $http.get('https://oc-run.firebaseio.com/lakeEvents/.json')
+    .then(function(response){
+      self.routes = response.data;
+      console.log(self.routes);
+    });
 
 })();
